16443db04ec6450bb6788f6c22e00d11e6dd7689
Tobias Reithmaier <tobias.0104.reithmaier@gmail.com>
2020-06-19 17:50:39 +0200

    Einzelne Kategorien sind nun klickbar, mit Klick listener im Viewholder. Anzeige durch einen Toast, welche Position geklickt wurde

--- app/src/main/java/com/example/stromtracker/ui/kategorien/KategorienListAdapter.kt
+++ app/src/main/java/com/example/stromtracker/ui/kategorien/KategorienListAdapter.kt
@@ -1,25 +1,45 @@
 package com.example.stromtracker.ui.kategorien
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.stromtracker.R
+import java.util.*
 
 class KategorienListAdapter(private val myDataset: Array<String>) : RecyclerView.Adapter<KategorienListAdapter.MyViewHolder>() {
-    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
     override fun onCreateViewHolder(parent: ViewGroup,
                                     viewType: Int): KategorienListAdapter.MyViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val textView = layoutInflater.inflate(R.layout.fragment_kategorien_recycler_text_view, parent, false) as TextView
+
         return MyViewHolder(textView)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.textView.text= myDataset[position]
-        holder.textView.text = myDataset[position]
     }
     override fun getItemCount() = myDataset.size
+
+    inner class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView), View.OnClickListener {
+
+        init {
+            textView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            if (v != null) {
+                Toast.makeText(v.context,
+                    String.format(
+                        Locale.GERMAN,
+                        "Position: %d is clicked.",
+                        layoutPosition),
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
 }
