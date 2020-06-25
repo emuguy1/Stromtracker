3fde73388858fbbc4443ca93a5928332c164658c
emuguy1 <emanuel.erben@gmail.com>
2020-06-25 23:04:49 +0200

    Commit to resume on a other device

---
+++ app/src/main/java/com/example/stromtracker/ui/raeume/ListAdapterRaeume.kt
@@ -0,0 +1,53 @@
+package com.example.stromtracker.ui.raeume
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.findFragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.stromtracker.R
+import com.example.stromtracker.ui.haushalt.ListAdapterHaushalt
+
+class ListAdapterRaeume() : RecyclerView.Adapter<ListAdapterHaushalt.ViewHolder>() {
+    private val data =
+        List(10) { ("Raum " + (it + 1).toString()) }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.list_item_raeume, parent, false)
+        return ViewHolder(view)
+    }
+
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item: String = data[position]
+        holder.textView.text = item
+        holder.textView
+
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+        var textView: TextView = itemView.findViewById(R.id.textview_haushalt)
+
+        init {
+            textView.setOnClickListener(this)
+        }
+
+        override fun onClick(view: View?) {
+            if (view != null) {
+                val frag = HaushaltBearbeitenLoeschenFragment()
+                //Fragment Manager aus Main Activity holen
+                val fragMan = view.findFragment<HaushaltFragment>().parentFragmentManager
+                //Wichtig: Hier bei R.id. die Fragment View aus dem content_main.xml auswählen! mit dem neuen Fragment ersetzen und dann committen.
+                fragMan.beginTransaction().replace(R.id.nav_host_fragment, frag)
+                    .addToBackStack(null).commit()
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/stromtracker/ui/raeume/RaeumeFragment.kt
+++ app/src/main/java/com/example/stromtracker/ui/raeume/RaeumeFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.stromtracker.R
-import com.example.stromtracker.ui.raeume.RaeumeViewModel
+import com.example.stromtracker.ui.haushalt.haushaltErstellen.HaushaltErstellenFragment
 
-class RaeumeFragment : Fragment() {
 
+//deklariert Haushaltfragment als Unterklasse von Fragment
+class HaushaltFragment: Fragment() {
     private lateinit var raeumeViewModel: RaeumeViewModel
 
     override fun onCreateView(
@@ -21,12 +23,35 @@ class RaeumeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         raeumeViewModel =
-            ViewModelProviders.of(this).get(com.example.stromtracker.ui.raeume.RaeumeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_raeume, container, false)
-        val textView: TextView = root.findViewById(R.id.text_raeume)
-        raeumeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+            ViewModelProviders.of(this).get(RaeumeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_haushalt, container, false)//false weil es nur teil des root ist, aber nicht selber die root
+
+        //Recyclerview, wo eine Liste aller Haushalte angezeigt wird. Alles weitere wird in ListAdapterHaushalt gesteuert
+        val recyclerView = root.findViewById<RecyclerView>(R.id.recyclerViewHaushalt)
+        recyclerView.layoutManager = LinearLayoutManager(this.context)
+        recyclerView.adapter = ListAdapterRaeume()
+
+        //Floating Action Button zum erstellen neuer Haushalte
+        //Floating actionbutton finden
+        val fab: View = root.findViewById(R.id.fab)
+        //Click listener setzen
+        fab.setOnClickListener { view ->
+            if (view != null) {
+                //neues Fragment erstellen auf das weitergeleitet werden soll
+                val frag = HaushaltErstellenFragment()
+                //Fragment Manager aus Main Activity holen
+                val fragMan = parentFragmentManager
+                //Ftagment container aus content_main.xml muss ausgeählt werden, dann mit neuen Fragment ersetzen, dass oben erstellt wurde
+                fragMan.beginTransaction().replace(R.id.nav_host_fragment, frag)
+                    .addToBackStack(null).commit();
+                //und anschließend noch ein commit()
+
+            }
+
+        }
         return root
     }
+
 }
+
+
