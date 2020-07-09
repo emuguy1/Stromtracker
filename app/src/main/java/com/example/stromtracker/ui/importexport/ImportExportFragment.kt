fe0a1446306b94abbe9aff7f6cd436d4277e1af0
emuguy1 <emanuel.erben@gmail.com>
2020-07-09 09:36:04 +0200

    File Picker wurde eingefügt. Allerdings sieht man noch nicht die anderen Ordner

--- app/src/main/java/com/example/stromtracker/ui/importexport/ImportExportFragment.kt
+++ app/src/main/java/com/example/stromtracker/ui/importexport/ImportExportFragment.kt
@@ -6,14 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.stromtracker.R
-import com.example.stromtracker.ui.importexport.ImportExportViewModel
+import yogesh.firzen.filelister.FileListerDialog
+import yogesh.firzen.filelister.OnFileSelectedListener
+import java.io.File
 
-class ImportExportFragment : Fragment() {
+
+class ImportExportFragment : Fragment(){
 
     private lateinit var importexportViewModel: ImportExportViewModel
+    private lateinit var pathselected:String
+    private lateinit var fileselected: File
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,12 +25,20 @@ class ImportExportFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         importexportViewModel =
-            ViewModelProviders.of(this).get(com.example.stromtracker.ui.importexport.ImportExportViewModel::class.java)
+            ViewModelProviders.of(this).get(ImportExportViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_importexport, container, false)
         val textView: TextView = root.findViewById(R.id.text_importExport)
-        importexportViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        val fileListerDialog: FileListerDialog= FileListerDialog.createFileListerDialog(this.requireContext())
+
+        fileListerDialog.setOnFileSelectedListener(OnFileSelectedListener { file, path ->
+            pathselected=path
+            fileselected=file
         })
+        fileListerDialog.setDefaultDir("/storage/emulated/0/Download")
+        fileListerDialog.setFileFilter(FileListerDialog.FILE_FILTER.ALL_FILES)
+        fileListerDialog.show()
         return root
     }
+
+
 }
--- app/src/main/java/com/example/stromtracker/ui/importexport/ImportExportViewModel.kt
+++ app/src/main/java/com/example/stromtracker/ui/importexport/ImportExportViewModel.kt
@@ -1,12 +1,61 @@
 package com.example.stromtracker.ui.importexport
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.stromtracker.database.*
 
-class ImportExportViewModel : ViewModel() {
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Import Export Fragment"
+class ImportExportViewModel(application: Application) : AndroidViewModel(application) {
+    var repo: DataRepository = DataRepository(application)
+    var geraetelist:LiveData<List<Geraete>> = repo.getAllGeraete()
+    var haushaltlist:LiveData<List<Haushalt>> = repo.getAllHaushalt()
+    var kategorieList:LiveData<List<Kategorie>> = repo.getAllKategorie()
+    var raumList:LiveData<List<Raum>> = repo.getAllRaeume()
+
+
+    fun getAllGeraete():LiveData<List<Geraete>> {
+        return geraetelist
     }
-    val text: LiveData<String> = _text
+
+
+
+    fun getAllKategorie():LiveData<List<Kategorie>> {
+        return kategorieList
+    }
+
+    fun insertKategorie(k: Kategorie) {
+        repo.insertKategorie(k)
+    }
+
+    fun getAllRaeume():LiveData<List<Raum>> {
+        return raumList
+    }
+
+    fun insertRaum(r: Raum) {
+        repo.insertRaum(r)
+    }
+
+    fun insertHaushalt(h: Haushalt) {
+        repo.insertHaushalt(h)
+    }
+
+    fun getAllHaushalt():LiveData<List<Haushalt>> {
+        return haushaltlist
+    }
+
+    fun insertGeraet(g: Geraete) {
+        repo.insertGeraete(g)
+    }
+
+    fun deleteGeraet(g: Geraete) {
+        repo.deleteGeraete(g)
+    }
+
+    fun updateGeraet(g: Geraete) {
+        repo.updateGeraete(g)
+    }
+
+
 }
