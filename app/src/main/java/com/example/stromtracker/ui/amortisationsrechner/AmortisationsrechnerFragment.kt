2be14c0bd0cd6e7d6e25bd601c8401c1d6cb5b20
Tobias Reithmaier <tobias@reithmaier.de>
2020-07-11 20:17:34 +0200

    Fixes for PullRequest #14 Amortisationsrechner into Develop
    Fixed lots of Naming Convention problems with Snake Case.
    Removed unnecessary Log.
    Splitted the large Callback Function for the custom Text Changed Listener in 3 smaller ones.
    Added several newlines at the end of a file.

--- app/src/main/java/com/example/stromtracker/ui/amortisationsrechner/AmortisationsrechnerFragment.kt
+++ app/src/main/java/com/example/stromtracker/ui/amortisationsrechner/AmortisationsrechnerFragment.kt
@@ -3,7 +3,6 @@ package com.example.stromtracker.ui.amortisationsrechner
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -42,7 +41,6 @@ class AmortisationsrechnerFragment : Fragment() {
         editAnschaffung = root.findViewById(R.id.amort_edit_anschaffung)
         editStrompreis = root.findViewById(R.id.amort_edit_strompreis)
 
-
         outKostenAkt = root.findViewById(R.id.amort_text_kosten_akt_num)
         outKostenNeu = root.findViewById(R.id.amort_text_kosten_neu_num)
         outAmortdauer = root.findViewById(R.id.amort_text_amortdauer_num)
@@ -61,51 +59,63 @@ class AmortisationsrechnerFragment : Fragment() {
             override fun afterTextChanged(s: Editable) {
                 val verbrAkt : Double? = editVerbrauchAkt.text.toString().toDoubleOrNull()
                 val verbrNeu : Double? = editVerbrauchNeu.text.toString().toDoubleOrNull()
-                val AK : Double? = editAnschaffung.text.toString().toDoubleOrNull()
+                val ak : Double? = editAnschaffung.text.toString().toDoubleOrNull()
                 val stromkosten : Double? = editStrompreis.text.toString().toDoubleOrNull()
-                Log.d("AMORTISATION", verbrAkt.toString() + verbrNeu.toString()+ AK.toString()+ stromkosten.toString())
-                var neuStr : String
-                if(stromkosten != null) {
-                    if (verbrAkt != null) {
-                        val aktDouble : Double = (verbrAkt * stromkosten / 100)
-                        neuStr = String.format("%.2f",aktDouble)+"€ "
-                        outKostenAkt.text = neuStr
-                    } else {
-                        outKostenAkt.text = null
-                    }
-                    if (verbrNeu != null) {
-                        val neuDouble : Double = (verbrNeu * stromkosten / 100)
-                        neuStr = String.format("%.2f",neuDouble)+"€ "
-                        outKostenNeu.text = neuStr
-                    } else {
-                        outKostenNeu.text = null
-                    }
-                    if(verbrAkt != null && verbrNeu != null && AK != null && verbrAkt > verbrNeu) {
-                        val amortDouble = (AK/((verbrAkt-verbrNeu) * stromkosten / 100))
-                        //Das Jahr wird immer auf ganze Zahlen abgerundet
-                        val df = DecimalFormat("#")
-                        df.roundingMode = RoundingMode.DOWN
-                        neuStr = df.format(amortDouble)+" Jahre und "+ String.format("%.1f", (amortDouble.rem(1)*365)) + " Tage bis zur Amortisation."
-                        outAmortdauer.text = neuStr
-                        neuStr = "Danach: " + String.format("%.2f", (verbrAkt-verbrNeu)*stromkosten/100) + "€ Ersparnis im Jahr."
-                        outAmortersparnis.text=neuStr
-                    }
-                    else {
-                        outAmortdauer.text = null
-                        outAmortersparnis.text = null
-                    }
-                }
-                else {
-                    outKostenAkt.text = null
-                    outKostenNeu.text = null
-                    outAmortdauer.text = null
-                    outAmortersparnis.text = null
-                }
+                updateOutputs(verbrAkt, verbrNeu, ak, stromkosten)
             }
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) { }
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) { }
         })
         return edit
+    }
+
+    fun updateOutputs (verbrAkt : Double?, verbrNeu : Double?, ak : Double?, stromkosten : Double?) {
+        var neuStr : String
+        if(stromkosten != null) {
+            if (verbrAkt != null) {
+                //Setzen der aktuellen Stromkosten pro Jahr
+                val aktDouble : Double = berechneStromkosten(stromkosten, verbrAkt)
+                neuStr = String.format("%.2f",aktDouble)+"€ "
+                outKostenAkt.text = neuStr
+            } else {
+                outKostenAkt.text = null
+            }
+            if (verbrNeu != null) {
+                //Setzen der neuen Stromkosten pro Jahr
+                val neuDouble : Double = berechneStromkosten(stromkosten, verbrNeu)
+                neuStr = String.format("%.2f",neuDouble)+"€ "
+                outKostenNeu.text = neuStr
+            } else {
+                outKostenNeu.text = null
+            }
+            if(verbrAkt != null && verbrNeu != null && ak != null && verbrAkt > verbrNeu) {
+                //Ausrechnen der Amortisationsdauer
+                val amortDouble = (ak / (berechneStromkosten (stromkosten, (verbrAkt-verbrNeu) ) ) )
+                //Das Jahr wird immer auf ganze Zahlen abgerundet
+                val df = DecimalFormat("#")
+                df.roundingMode = RoundingMode.DOWN
+                //Ausgabe der Dauer in Jahren und Tagen
+                neuStr = df.format(amortDouble)+" Jahre und "+ String.format("%.1f", (amortDouble.rem(1)*365)) + " Tage bis zur Amortisation."
+                outAmortdauer.text = neuStr
+                //Ausrechnen der Ersparnis nach Amortisation
+                neuStr = "Danach: " + String.format("%.2f", berechneStromkosten(stromkosten, (verbrAkt-verbrNeu))) + "€ Ersparnis im Jahr."
+                outAmortersparnis.text=neuStr
+            }
+            else {
+                outAmortdauer.text = null
+                outAmortersparnis.text = null
+            }
+        }
+        else {
+            outKostenAkt.text = null
+            outKostenNeu.text = null
+            outAmortdauer.text = null
+            outAmortersparnis.text = null
+        }
+    }
+
+    fun berechneStromkosten(stromkosten : Double, verbrauch : Double) : Double {
+        return (verbrauch * stromkosten / 100)
     }
 
 }
