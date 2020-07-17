40290e1b739a9719f848cb427e887934efab000f
Tobias Reithmaier <tobias.0104.reithmaier@gmail.com>
2020-07-17 08:34:14 +0200

    Fix: Das Programm stürzt nicht mehr ab, wenn kein Datum als String eingegeben wird Der ganze Block wurde mit try, catch umschlossen um den Fehler gezielt zu behandeln. Das Log ist beabsichtigt und wird anstatt dem StackTrace ausgegeben

--- app/src/main/java/com/example/stromtracker/ui/urlaub/UrlaubCompanion.kt
+++ app/src/main/java/com/example/stromtracker/ui/urlaub/UrlaubCompanion.kt
@@ -1,6 +1,8 @@
 package com.example.stromtracker.ui.urlaub
 
+import android.util.Log
 import android.widget.EditText
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -17,14 +19,20 @@ class UrlaubCompanion {
             if (st.text.isNotEmpty() && end.text.isNotEmpty()) {
                 if (st.text.length == this.dateLength && end.text.length == dateLength) {
                     //TODO String richtig überprüfen ob er wirklich ein gültiges Datum ist, ggf. try - catch
-                    val tempSt = SimpleDateFormat(
-                        "dd.MM.yyyy",
-                        Locale.GERMAN
-                    ).parse(st.text.toString())
-                    val tempEnd = SimpleDateFormat(
-                        "dd.MM.yyyy",
-                        Locale.GERMAN
-                    ).parse(end.text.toString())
+                    val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.GERMAN)
+                    var tempSt: Date? = null
+                    var tempEnd: Date? = null
+                    try {
+                        tempSt = dateFormat.parse(st.text.toString())
+                        tempEnd = dateFormat.parse(end.text.toString())
+                    } catch (e: ParseException) {
+                        Log.d(
+                            "ERROR_StringIsNoDate",
+                            "Der eingegebene String ist kein Datum und lässt sich somit nicht konvertieren."
+                        )
+                        //e.printStackTrace() //um die Fehlermeldung auf dem StackTrace auszugeben
+                        return false
+                    }
                     if (tempSt != null && tempEnd != null) {
                         if (tempSt.before(tempEnd) || tempSt == tempEnd)
                             return true
