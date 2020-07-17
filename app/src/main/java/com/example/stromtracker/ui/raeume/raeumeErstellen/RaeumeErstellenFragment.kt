0f82a240da64b5c8c60fffcef52fa659a465233b
emuguy1 <emanuel.erben@gmail.com>
2020-07-17 10:35:16 +0200

    Es wurde geändert, dass keine Räume ohne Name erzeugt werden können und dass der Name nicht nur aus leerzeichen bestehen darf

--- app/src/main/java/com/example/stromtracker/ui/raeume/raeumeErstellen/RaeumeErstellenFragment.kt
+++ app/src/main/java/com/example/stromtracker/ui/raeume/raeumeErstellen/RaeumeErstellenFragment.kt
@@ -9,10 +9,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.stromtracker.R
@@ -26,6 +23,7 @@ class RaeumeErstellenFragment : Fragment() {
     private lateinit var raeumeViewModel: RaeumeViewModel
     private lateinit var raumnameneditfeld: EditText
     private lateinit var savebutton: View
+    private lateinit var informationfield: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,6 +35,7 @@ class RaeumeErstellenFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_raeumeerstellen, container, false)
         raumnameneditfeld = root.findViewById<EditText>(R.id.edit_text_raum_erstellen_name)
         CustomTextListener(raumnameneditfeld)
+        informationfield=root.findViewById<TextView>(R.id.edit_text_raum_erstellen_info)
 
         //Speicher Button zum speichern der eingegebenen Daten
         //finde den save button
@@ -44,24 +43,28 @@ class RaeumeErstellenFragment : Fragment() {
         //Click listener setzen
         savebutton.setOnClickListener { view ->
             if (view != null) {
-                //Die Daten in die RoomDatabase speichern
+                if(!raumnameneditfeld.text.isNullOrBlank()){
+                    //Die Daten in die RoomDatabase speichern
+                    val navView = requireActivity().findViewById<NavigationView>(R.id.nav_view)
 
+                    val sp: Spinner = navView.menu.findItem(R.id.nav_haushalt).actionView as Spinner
+                    val currHaushalt = sp.selectedItem as Haushalt
 
-                val navView = requireActivity().findViewById<NavigationView>(R.id.nav_view)
+                    val raum: Raum =
+                        Raum(raumnameneditfeld.text.toString(), currHaushalt.getHaushaltID())
+                    raeumeViewModel.insertRaeume(raum)
+                    //neues Fragment erstellen auf das weitergeleitet werden soll
+                    val frag = RaeumeFragment()
+                    //Fragment Manager aus Main Activity holen
+                    val fragMan = parentFragmentManager
+                    //Ftagment container aus content_main.xml muss ausgeählt werden, dann mit neuen Fragment ersetzen, dass oben erstellt wurde
+                    fragMan.beginTransaction().replace(R.id.nav_host_fragment, frag).commit();
+                    //und anschließend noch ein commit()
+                }
+                else{
+                    informationfield.text = "Raum kann nicht gespeichert werden, da Kein Name eingegeben wurde"
+                }
 
-                val sp: Spinner = navView.menu.findItem(R.id.nav_haushalt).actionView as Spinner
-                val currHaushalt = sp.selectedItem as Haushalt
-
-                val raum: Raum =
-                    Raum(raumnameneditfeld.text.toString(), currHaushalt.getHaushaltID())
-                raeumeViewModel.insertRaeume(raum)
-                //neues Fragment erstellen auf das weitergeleitet werden soll
-                val frag = RaeumeFragment()
-                //Fragment Manager aus Main Activity holen
-                val fragMan = parentFragmentManager
-                //Ftagment container aus content_main.xml muss ausgeählt werden, dann mit neuen Fragment ersetzen, dass oben erstellt wurde
-                fragMan.beginTransaction().replace(R.id.nav_host_fragment, frag).commit();
-                //und anschließend noch ein commit()
 
             }
 
@@ -93,8 +96,12 @@ class RaeumeErstellenFragment : Fragment() {
             override fun afterTextChanged(s: Editable) {
                 if (edit.text.toString().equals("Sonstiges")) {
                     savebutton.visibility = View.INVISIBLE
-                    Toast.makeText(edit.context,"Raum - Sonstiges - darf nicht erstellt werden!",Toast.LENGTH_SHORT).show()
-                } else {
+                    informationfield.text="Raum - Sonstiges - darf nicht erstellt werden!"
+                }else if(edit.text.toString().isBlank()){
+                    savebutton.visibility=View.INVISIBLE
+                    informationfield.text="Es muss ein Name eingegeben werden, der Nicht aus Leerzeichen besteht oder leer ist!"
+                }
+                else {
                     savebutton.visibility = View.VISIBLE
                 }
             }
