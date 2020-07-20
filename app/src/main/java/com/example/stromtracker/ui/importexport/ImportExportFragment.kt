bed81a928a6b455736c7b8e3b4c920c6bc9d33b7
emuguy1 <emanuel.erben@gmail.com>
2020-07-20 09:35:14 +0200

    Rebuild to new Version of csvwriter

--- app/src/main/java/com/example/stromtracker/ui/importexport/ImportExportFragment.kt
+++ app/src/main/java/com/example/stromtracker/ui/importexport/ImportExportFragment.kt
@@ -1,7 +1,6 @@
 package com.example.stromtracker.ui.importexport
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,19 +11,13 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.example.stromtracker.R
 import com.example.stromtracker.database.Haushalt
-import com.github.doyaaaaaken.kotlincsv.client.CsvWriter
-
 import java.io.File
-import java.io.FileWriter
-import java.nio.charset.Charset
-import java.nio.charset.StandardCharsets.UTF_8
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.text.Charsets.UTF_8
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 
 
 class ImportExportFragment : Fragment(){
@@ -39,7 +32,7 @@ class ImportExportFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         importexportViewModel =
-            ViewModelProviders.of(this).get(ImportExportViewModel::class.java)
+            ViewModelProvider(this).get(com.example.stromtracker.ui.importexport.ImportExportViewModel::class.java)
         haushaltlist=ArrayList()
         importexportViewModel.getAllHaushalt().observe(
             viewLifecycleOwner,
@@ -50,9 +43,6 @@ class ImportExportFragment : Fragment(){
                 }
             }
         )
-
-
-            ViewModelProvider(this).get(com.example.stromtracker.ui.importexport.ImportExportViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_importexport, container, false)
         val textView: TextView = root.findViewById(R.id.text_importExport)
         val exportbut: Button =root.findViewById(R.id.importexport_exportbutton)
@@ -61,7 +51,7 @@ class ImportExportFragment : Fragment(){
                 val csvFile = generateFile(requireContext(),"HaushaltRoomExport.csv" )
                 if (csvFile != null) {
                     //exportDatabaseToCSVFile(csvFile)
-                    CsvWriter().open(csvFile,append=false){
+                    csvWriter().open(csvFile,append=false){
                         writeRow(listOf("[HaushaltIdid]","[Name]","[Stromkosten]","[bewohnerAnzahl]","[zaehlerstand]","[datum]","[oekostrom]"))
                         haushaltlist.forEachIndexed{index,haushalt->
                             if(haushalt.getDatum()!=null) {
@@ -73,6 +63,7 @@ class ImportExportFragment : Fragment(){
                                         haushalt.getBewohnerAnzahl(),
                                         haushalt.getZaehlerstand(),
                                         SimpleDateFormat("dd.MM.yyyy", Locale.GERMAN).format(
+                                            //Kann nicht null sein, da danach geprüft wird.
                                             haushalt.getDatum()
                                         ),
                                         haushalt.getOekostrom()
@@ -94,7 +85,7 @@ class ImportExportFragment : Fragment(){
                             }
                         }
                     }
-                    Toast.makeText(this.context, "CSV File wurde Erstellet"+csvFile.readText(), Toast.LENGTH_LONG).show()
+                    Toast.makeText(this.context, "CSV File wurde Erstellet"+csvFile.readText(), Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(this.context, "CSV File konnte nicht erstellt werden", Toast.LENGTH_LONG).show()
                 }
