88cead057ed0af1a4afd0ae7cd91db6fc88e1596
emuguy1 <emanuel.erben@gmail.com>
2020-07-21 15:03:30 +0200

    Started doing the import with the given File of the Export.

--- app/src/main/java/com/example/stromtracker/ui/importexport/ImportExportFragment.kt
+++ app/src/main/java/com/example/stromtracker/ui/importexport/ImportExportFragment.kt
@@ -2,10 +2,7 @@ package com.example.stromtracker.ui.importexport
 
 import android.content.Context
 import android.content.Intent
-import android.media.session.MediaController
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,13 +18,13 @@ import com.example.stromtracker.database.Geraete
 import com.example.stromtracker.database.Haushalt
 import com.example.stromtracker.database.Kategorie
 import com.example.stromtracker.database.Raum
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import java.io.File
+import java.io.FileReader
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
-import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
 
 
 class ImportExportFragment : Fragment() {
@@ -37,20 +34,10 @@ class ImportExportFragment : Fragment() {
     private lateinit var raumlist: ArrayList<Raum>
     private lateinit var kategorielist: ArrayList<Kategorie>
     private lateinit var geraetlist: ArrayList<Geraete>
-    private lateinit var csvfile : File
-//    private lateinit var urlaublist: ArrayList<Urlaub>
+    private lateinit var csvfile: File
 
-
-    private val filepath = "MyFileStorage"
-    internal var myExternalFile: File?=null
-    private val isExternalStorageReadOnly: Boolean get() {
-         val extStorageState = Environment.getExternalStorageState()
-         return Environment.MEDIA_MOUNTED_READ_ONLY == extStorageState
-    }
-    private val isExternalStorageAvailable: Boolean get() {
-        val extStorageState = Environment.getExternalStorageState()
-        return Environment.MEDIA_MOUNTED == extStorageState
-    }
+    //    private lateinit var urlaublist: ArrayList<Urlaub>
+    private lateinit var haushaltidlist: ArrayList<Int>
 
 
     override fun onCreateView(
@@ -72,9 +59,9 @@ class ImportExportFragment : Fragment() {
 
         }
 
-        val importbut:Button = root.findViewById(R.id.importexport_importbutton)
-        importbut.setOnClickListener{view ->
-            if(view!=null){
+        val importbut: Button = root.findViewById(R.id.importexport_importbutton)
+        importbut.setOnClickListener { view ->
+            if (view != null) {
 
             }
         }
@@ -83,14 +70,159 @@ class ImportExportFragment : Fragment() {
         return root
     }
 
-//    private fun getDatafromcsv(csv:File){
-//        csv.absolutePath
-//        Toast.makeText(
-//            this.context,
-//            "CSV File wurde Erstellet" + csv.absolutePath,
-//            Toast.LENGTH_SHORT
-//        ).show()
-//    }
+    private fun getDatafromcsv(csv: File) {
+        var zustand = 0
+        var headerread = false
+        val daten: ArrayList<String> = ArrayList()
+        FileReader(csv).forEachLine { row: String ->
+            daten.add(row)
+            //Zusand 0 sind die Haushalte
+            if (zustand == 0) {
+                var akthaushaltid = haushaltlist[haushaltlist.size - 1].getHaushaltID()
+                var haushaltzaehler = 0
+                haushaltidlist = ArrayList()
+                if (!headerread) {
+                    headerread = true
+                } else {
+                    if (row == "-----------------------------------") {
+                        zustand = 1
+                        headerread = false
+                    } else {
+                        val data = row.split(",")
+                        //TODO: Haushaltids holen, aber eher über eine erneute Abfrage der Datenbank nach einfügen
+                        haushaltidlist.add(data[0].toInt())
+                        haushaltzaehler++
+                        //haushaltidlist[0][1]=akthaushaltid
+                        //akthaushaltid+=1
+                        //restlichen Haushaltdaten speichern und dann zu einem Haushalt hinzufügen
+                        if (data[5].isNotEmpty()) {
+                            //try catch Block um Parser Fehler beim Datum abzufangen
+                            try {
+                                val haushaltname: String = data[1]
+                                val strompreis = data[2].toDouble()
+                                val personenanzahl = data[3].toInt()
+                                val zaehlerstand = data[4].toDoubleOrNull()
+                                val datum = SimpleDateFormat(
+                                    "dd.MM.yyyy",
+                                    Locale.GERMAN
+                                ).parse(data[5])
+                                val oekomix = data[6].toBoolean()
+                                val tempHaushalt = Haushalt(
+                                    haushaltname,
+                                    strompreis,
+                                    personenanzahl,
+                                    zaehlerstand,
+                                    datum,
+                                    oekomix
+                                )
+                                importexportViewModel.insertHaushalt(tempHaushalt)
+                            } catch (e: ParseException) {
+                                Toast.makeText(
+                                    this.context,
+                                    R.string.parse_error_datum,
+                                    Toast.LENGTH_SHORT
+                                )
+                                    .show()
+                                e.printStackTrace()
+                            }
+
+                        } else {
+                            val haushaltname: String = data[1]
+                            val strompreis = data[2].toDouble()
+                            val personenanzahl = data[3].toInt()
+                            val oekomix = data[4].toBoolean()
+                            val tempHaushalt = Haushalt(
+                                haushaltname,
+                                strompreis,
+                                personenanzahl,
+                                null,
+                                null,
+                                oekomix
+                            )
+                            importexportViewModel.insertHaushalt(tempHaushalt)
+                        }
+
+                    }
+                }
+                //Hier müsste das holen der Haushaltids passieren
+                //Überlegung, ob man nicht bei Update auf ein Feld der MainActivity zugreifen könnte, wo die neuen IDs gespeichert werden
+                //Oder ein Art Import Wizzard, wo man durch die Fragments durch gerauscht wird, um den selben Trick zu verwenden, wie bei dem erstellen der Standardräume
+            }
+            //Räume
+            else if (zustand == 1) {
+                if (!headerread) {
+                    headerread = true
+                } else {
+                    if (row == "-----------------------------------") {
+                        zustand = 2
+                        headerread = false
+                    } else {
+                        val data = row.split(",")
+
+
+                    }
+                }
+                //Hier müsste das holen des Raum passieren
+            }
+            //Kategorie
+            else if (zustand == 2) {
+                if (!headerread) {
+                    headerread = true
+                } else {
+                    if (row == "-----------------------------------") {
+                        zustand = 3
+                        headerread = false
+                    } else {
+                        val data = row.split(",")
+                        //TODO: besonderheit, dass Kategorien nicht doppelt erzeugt werden sollen. das heißt hier mit alter Liste vergleichen
+
+                    }
+                }
+                //Hier müsste das holen der Kategorie passieren
+            }
+            //Geräte
+            else if (zustand == 3) {
+                if (!headerread) {
+                    headerread = true
+                } else {
+                    if (row == "-----------------------------------") {
+                        zustand = 4
+                        headerread = false
+                    } else {
+                        val data = row.split(",")
+
+
+                    }
+                }
+                //Hier müsste das holen des Geräte passieren
+            }
+            //Urlaub
+            else if (zustand == 4) {
+                if (!headerread) {
+                    headerread = true
+                } else {
+                    if (row == "-----------------------------------") {
+                        zustand = 5
+                        headerread = false
+                    } else {
+                        val data = row.split(",")
+
+
+                    }
+                }
+                //Hier müsste das holen des Urlaubs passieren
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    row,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+
+    }
+
 
     private fun writeCSVFile() {
         val csvFile = generateFile()
@@ -226,8 +358,9 @@ class ImportExportFragment : Fragment() {
                 "CSV File wurde Erstellet" + csvFile.readText(),
                 Toast.LENGTH_SHORT
             ).show()
-            val intent = goToFileIntent(requireContext(), csvFile)
-            startActivity(intent)
+            //TODO : für besseres Testen getDatafromcsv(csvFile)
+            //val intent = goToFileIntent(requireContext(), csvFile)
+            //startActivity(intent)
 
         } else {
             Toast.makeText(
@@ -239,7 +372,7 @@ class ImportExportFragment : Fragment() {
         }
     }
 
-    private fun createLists(){
+    private fun createLists() {
         haushaltlist = ArrayList()
         raumlist = ArrayList()
         kategorielist = ArrayList()
@@ -294,7 +427,7 @@ class ImportExportFragment : Fragment() {
     }
 
     private fun generateFile(): File? {
-        val csvFile=File(requireContext().filesDir,"StromtrackerRoomExport.csv")
+        val csvFile = File(requireContext().filesDir, "StromtrackerRoomExport.csv")
         csvFile.createNewFile()
 
         return if (csvFile.exists()) {
@@ -306,10 +439,12 @@ class ImportExportFragment : Fragment() {
 
     private fun goToFileIntent(context: Context, file: File): Intent {
         val intent = Intent(Intent.ACTION_VIEW)
-        val contentUri = FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", file)
+        val contentUri =
+            FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", file)
         val mimeType = context.contentResolver.getType(contentUri)
         intent.setDataAndType(contentUri, mimeType)
-        intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+        intent.flags =
+            Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
 
         return intent
     }
