099ba66fa7f85996f82273e955de1cc5bc75b569
Matteo Hoffmann <hoffmatteo@gmail.com>
2020-07-23 09:48:37 +0200

    Finished home fragment

--- app/src/main/java/com/example/stromtracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/stromtracker/ui/home/HomeFragment.kt
@@ -16,15 +16,6 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.home_text_def)
-        val text: String =
-            "Die App soll den Stromverbrauch eines Haushaltes erfassen, visualisieren und Kosten und Nutzen einzelner Geräte und des gesamten Haushaltes errechnen. Hierzu muss der User selbst die Verbrauchsdaten seiner Geräte messen/ablesen und in die App eintragen. Außerdem beinhaltet die App Amortisationsrechner, um die Kostenvorteile von Solaranlagen oder effizienteren Geräten abzuschätzen.\n" +
-                    "\n" +
-                    "Es werden ein Standardhaushalt mit mehreren Standardräumen, sowie mehrere Standardkategorien erstellt. Falls ein Haushalt gelöscht wird, werden alle darin enthaltenen Räume und Geräte ebenfalls gelöscht.\n" +
-                    "\n" +
-                    "By Tobias Reithmaier Matteo Hoffmann Emanuel Erben"
-        textView.text = text
-
         return root
     }
 }
